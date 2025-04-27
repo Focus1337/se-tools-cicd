1fca9015b3b38ee21101f688b698e209f7b65af3
Focus <maniaclord@bk.ru>
2025-04-27 22:18:48 +0300

    qq

--- dotnet-web-api/dotnet-web-api/Program.cs
+++ dotnet-web-api/dotnet-web-api/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5251);
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
